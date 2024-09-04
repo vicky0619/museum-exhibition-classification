4fe177d3a9d1972b0e4c6de3c7c861c32d2e2e42
vicky0619 <vicky46586038@gmail.com>
2024-09-04 16:23:17 +0800

    xocde

---
+++ museum_classification/Pods/Target Support Files/Pods-museum_classification-museum_classificationUITests/Pods-museum_classification-museum_classificationUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_museum_classification_museum_classificationUITests : NSObject
+@end
+@implementation PodsDummy_Pods_museum_classification_museum_classificationUITests
+@end
---
+++ museum_classification/museum_classification/museum_classificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  museum_classificationApp.swift
+//  museum_classification
+//
+//  Created by Vicky T on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct museum_classificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
